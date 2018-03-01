9f539e5e6d6145aa129795471d78e4164b61be7f
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-01 12:38:17 +0530

    Added take_a_break.py

---
+++ take_a_break.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while break_count < total_breaks:
+    time.sleep(10)
+    webbrowser.open("https://youtu.be/D5IlAZ87T9c")
+    break_count = break_count + 1
+
