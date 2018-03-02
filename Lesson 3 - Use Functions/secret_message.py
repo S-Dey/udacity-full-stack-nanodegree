a680135ff0e4a2e4b28ba16964b433adcf0cfd6c
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-03 01:08:22 +0530

    Bug fixes

--- Lesson 3 - Use Functions/secret_message.py
+++ Lesson 3 - Use Functions/secret_message.py
@@ -3,15 +3,16 @@ import os
 def rename_files():
     #(1) Get filenames from a directory/folder
     file_list = os.listdir(r"E:\Full Stack Web Developer Nanodegree\Lesson 3 - Use Functions\prank")
-    saved_path = os.getcwd()        #We have to use it since the `os.rename()` method renames files in the current directory only.
-                                    #But, we want to change the names of files in the `prank` directory.  
+    saved_path = os.getcwd()        #We have to use it, since the `os.rename()` method renames files inside the current directory.
+                                    #But, we want to change the names of files inside the `prank` directory.  
     os.chdir(r"E:\Full Stack Web Developer Nanodegree\Lesson 3 - Use Functions\prank")
 
     #(2) Rename all the files in the 'prank' directory
     for file_name in file_list:
+        translation_table = str.maketrans("0123456789", "          ", "0123456789")
         print("Old name - " + file_name)
-        print("New name - " + file_name.translate(None, "0123456789"))
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        print("New name - " + file_name.translate(translation_table) + "\n")
+        os.rename(file_name, file_name.translate(translation_table))
     
     os.chdir(saved_path)            #Going back to the previously working directory.
 
