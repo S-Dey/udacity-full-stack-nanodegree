34b6536b2c0326ef418dcac09f670342d80627e0
Subhadeep Dey <SDey96@users.noreply.github.com>
2018-03-03 23:20:27 +0530

    Update send_text.py

--- Lesson 5/send_text.py
+++ Lesson 5/send_text.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = "AC3e4c8a064ab530f38f73736fd182d5d9"
-auth_token = "608293f8d3423704f19488dda88e178bc"
+account_sid = "Your account SID"
+auth_token = "Your authentication token"
 client = Client(account_sid, auth_token)
 client.messages.create(
   to="+91950xxxxx",     #Change to your own.
