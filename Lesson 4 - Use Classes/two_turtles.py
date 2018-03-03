3d62d6e578069648d8b4bff9101f5a512dd2f204
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-03 19:20:35 +0530

    Optimized code.

---
+++ Lesson 4 - Use Classes/two_turtles.py
@@ -0,0 +1,29 @@
+#Python program to draw a square on screen
+
+import turtle
+
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("orange")
+
+    #Create the turtle 'Brad' - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    draw_square(brad)
+
+    #Create the turtle 'Angie' - Draws a circle
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+
+    window.exitonclick()
+
+draw_art()
