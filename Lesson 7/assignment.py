36b1814059fbf9835c62df2b4eb3ac31ddb7c40b
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-05 01:46:09 +0530

    Added assignment.

---
+++ Lesson 7/assignment.py
@@ -0,0 +1,5 @@
+import media
+
+#Creating instances
+blade_runner_2049 = media.Movie("Blade Runner 2049", "Set in future; retiring replicants.", "http://www.impawards.com/2017/blade_runner_twenty_forty_nine_ver4_xlg.html", "https://youtu.be/gCcx85zbxz4")
+blade_runner_2049.show_trailer()
