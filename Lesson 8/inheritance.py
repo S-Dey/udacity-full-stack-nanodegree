9a15f7d8a5bb206d63337e1bff7f06f01e156ab8
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-05 22:59:12 +0530

    Improvements.

--- Lesson 8/inheritance.py
+++ Lesson 8/inheritance.py
@@ -1,12 +1,12 @@
 class Parent:
     def __init__(self, last_name, eye_color):
         print("Parent constructor called.")
-        self.last_name = last_name
-        self.eye_color = eye_color
+        self.last_name_parent = last_name
+        self.eye_color_parent = eye_color
     
     def show_info(self):
-        print("Last name - " + self.last_name)
-        print("Eye colour - " + self.eye_color)
+        print("Last name - " + self.last_name_parent)
+        print("Eye colour - " + self.eye_color_parent)
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -15,11 +15,12 @@ class Child(Parent):
         self.number_of_toys = number_of_toys
 
     def show_info(self):
-        print("Last name - " + self.last_name)
-        print("Eye colour - " + self.eye_color)
+        print("Last name - " + self.last_name_parent)
+        print("Eye colour - " + self.eye_color_parent)
         print("Number of toys - " + str(self.number_of_toys))
 
 billy_cyrus = Parent("Cyrus", "Blue")
 miley_cyrus = Child("Cyrus", "Blue", 5)
-print(miley_cyrus.last_name)
+print(miley_cyrus.last_name_parent)
 miley_cyrus.show_info()
+billy_cyrus.show_info()
