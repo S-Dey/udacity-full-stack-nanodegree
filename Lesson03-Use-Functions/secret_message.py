4a4c88a20793931fcc17e9be9359f8d1a98831cd
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-17 23:46:33 +0530

    Optimized code.

--- Lesson03-Use-Functions/secret_message.py
+++ Lesson03-Use-Functions/secret_message.py
@@ -1,16 +1,19 @@
 import os
 
+
 def rename_files():
-    #(1) Get filenames from the directory called "prank/"
+    # (1) Get filenames from the directory called "prank/"
     file_list = os.listdir(r"E:\Full Stack Web Developer Nanodegree\Lesson 3 - Use Functions\prank")
-    saved_path = os.getcwd()        #We have to use it, since the `os.rename()` method renames files inside the current directory.
-                                    #But, we want to change the names of files inside the `prank` directory.  
+
+    # We are using it since the `os.rename()` method renames files inside the current directory.
+    # But, we want to change the names of files inside the `prank` directory.
+    saved_path = os.getcwd()         
     os.chdir(r"E:\Full Stack Web Developer Nanodegree\Lesson 3 - Use Functions\prank")
 
-    #(2) Rename all the files in the directory "prank/"
+    # (2) Rename all the files in the directory "prank/"
     for file_name in file_list:
-        translation_table = str.maketrans("0123456789", "          ", "0123456789")     #Each of the numeric digit (as in first argument)
-                                                                                        # would be replaced with a whitespace. 
+        # Each of the numeric digit (as in first argument) would be replaced with a whitespace. 
+        translation_table = str.maketrans("0123456789", "          ", "0123456789")                                                                                       
         print("Old name - " + file_name)
         print("New name - " + file_name.translate(translation_table) + "\n")
         os.rename(file_name, file_name.translate(translation_table))
