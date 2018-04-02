f5364dd8a3108cf6411ebbddb9f981aa524426ea
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-03 02:36:35 +0530

    Added docstrings.

--- Lesson07-Make-Classes-Movie-Website/media.py
+++ Lesson07-Make-Classes-Movie-Website/media.py
@@ -2,6 +2,8 @@ import webbrowser
 
 
 class Movie:
+    """ Class defining movie attributes. """
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
