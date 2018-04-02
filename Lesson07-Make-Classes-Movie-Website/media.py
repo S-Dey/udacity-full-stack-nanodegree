4796da39bf46836f43d1ceb5f672eaf33fc5665e
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-03 02:34:49 +0530

    Compliance with PEP8 coding style.

--- Lesson07-Make-Classes-Movie-Website/media.py
+++ Lesson07-Make-Classes-Movie-Website/media.py
@@ -1,7 +1,9 @@
 import webbrowser
 
+
 class Movie:
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
