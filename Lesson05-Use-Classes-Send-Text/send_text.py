02d7b1eee0c61b9c44dc8c79373ac24cc8618b0d
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-03 02:14:45 +0530

    Modified coding styles.

--- Lesson05-Use-Classes-Send-Text/send_text.py
+++ Lesson05-Use-Classes-Send-Text/send_text.py
@@ -4,7 +4,7 @@ account_sid = "Your account SID"
 auth_token = "Your authentication token"
 client = Client(account_sid, auth_token)
 client.messages.create(
-  to="+91950xxxxx",     #Change to your own.
-  from_="+1720xxxxx",   #Change to your own.
+  to="+91950xxxxx",     # Change to your own.
+  from_="+1720xxxxx",   # Change to your own.
   body="Tomorrow's forecast in Financial District, San Francisco is Clear",
   media_url="https://climacons.herokuapp.com/clear.png")
