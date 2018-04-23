1f03d152f832712d51256f3ed0267968e8fb29b1
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-23 19:30:22 +0530

    Followed PEP8 Style Guidelines.

--- Unit-01/Lesson06-Profanity-Editor/curse_word_detector.py
+++ Unit-01/Lesson06-Profanity-Editor/curse_word_detector.py
@@ -1,17 +1,21 @@
 import urllib.request as urllib
 
+
 def read_text():
     text_to_read = open("some_text.txt")
     content = text_to_read.read()
     text_to_read.close()
     check_profanity(content)
 
+
 def check_profanity(text_to_check):
-    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)       #Unfortunately, it works
-                                                        #with only a single word. Writing more words would result
-                                                        #into an 404 error.
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="
+                                + text_to_check)
+    # Unfortunately, it works with only a single word. Writing more words
+    # would result into an 404 error.
     output = connection.read()
     print(output)
     connection.close()
+
 
 read_text()
