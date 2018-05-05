29d7714ddf8807b2eac8ce43d2f01fca41cf835f
Subhadeep Dey <contact.sdey@gmail.com>
2018-05-05 22:33:15 +0530

    Add more modules.

--- Term II/Unit03-The-Backend--Applications/Lesson03-Developing-with-frameworks/project-2.py
+++ Term II/Unit03-The-Backend--Applications/Lesson03-Developing-with-frameworks/project-2.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
-
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -23,9 +22,16 @@ def restaurantMenu(restaurant_id):
 # Task 1: Create route for newMenuItem function here
 
 
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
