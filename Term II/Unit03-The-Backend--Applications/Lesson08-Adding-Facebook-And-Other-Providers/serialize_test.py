95813b0ecf957478992ce30fd0d00a95aa32efea
Subhadeep Dey <contact.sdey@gmail.com>
2018-06-30 12:12:52 +0530

    Add more files

---
+++ Term II/Unit03-The-Backend--Applications/Lesson08-Adding-Facebook-And-Other-Providers/serialize_test.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup_with_users import Restaurant, Base, MenuItem, User
+
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+items = session.query(MenuItem).order_by(MenuItem.id.desc())
+
+# Reurns dictionary of each item
+for i in items:
+    print(i.serialize)
