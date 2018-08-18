b3e1febc74f2534f0de2a654ca40a32a1cb1f782
Subhadeep Dey <contact.sdey@gmail.com>
2018-08-18 17:59:38 +0530

    Follow PEP-8 Guidelines

--- Term I/Unit-01/Lesson03-Use-Functions/secret_message.py
+++ Term I/Unit-01/Lesson03-Use-Functions/secret_message.py
@@ -7,18 +7,21 @@ def rename_files():
 
     # We are using it since the `os.rename()` method renames files inside the current directory.
     # But, we want to change the names of files inside the `prank` directory.
-    saved_path = os.getcwd()         
+    saved_path = os.getcwd()
     os.chdir(r"E:\Full Stack Web Developer Nanodegree\Lesson 3 - Use Functions\prank")
 
     # (2) Rename all the files in the directory "prank/"
     for file_name in file_list:
-        # Each of the numeric digit (as in first argument) would be replaced with a whitespace. 
-        translation_table = str.maketrans("0123456789", "          ", "0123456789")
+        # Each of the numeric digit (as in first argument) would be replaced
+        # with a whitespace.
+        translation_table = str.maketrans("0123456789", "          ",
+                                          "0123456789")
         print("Old name - " + file_name)
         print("New name - " + file_name.translate(translation_table) + "\n")
         os.rename(file_name, file_name.translate(translation_table))
-    
-    #Going back to the previously working directory.
+
+    # Going back to the previously working directory.
     os.chdir(saved_path)
+
 
 rename_files()
